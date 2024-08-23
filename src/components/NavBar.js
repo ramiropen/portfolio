b9045c0a08d793679f267127f46366153b6a8437
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2024-08-22 22:11:41 -0700

    more changes to the nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,50 @@
-import {Navbar, Container } from 'react-bootstrap';
+import {useState, useEffect} from 'react';
+import {Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../logo.svg';
+//I still need to get icons and img's, import also----------------------->
 
 export const NavBar = () => {
+    const [activelink, setActivelink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
-            <Navbar.Brand href="#home">NavBar</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand href="#home">
+                <img src={'https://react-bootstrap.github.io/logo.svg'} alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="#home" className={activelink === 'home' ? 'active navbar-link' : 'navbar-link'} onCLick={() => setActivelink('home')}>Home</Nav.Link>
+                <Nav.Link href="#skills" className={activelink === 'skills' ? 'active navbar-link' : 'navbar-link'} onCLick={() => setActivelink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activelink === 'projects' ? 'active navbar-link' : 'navbar-link'} onCLick={() => setActivelink('projects')}>Projects</Nav.Link>
             </Nav>
+            <span className="navbar-text">
+                <div className="social-icon">
+                    <a href="#"><img src={'https://react-bootstrap.github.io/logo.svg'} alt="Logo" /></a>
+                    <a href="#"><img src={'https://react-bootstrap.github.io/logo.svg'} alt="Logo" /></a>
+                    <a href="#"><img src={'https://react-bootstrap.github.io/logo.svg'} alt="Logo" /></a>
+                </div>
+                <button className="vvd" onCLick={() => console.log('connect')}><span>Let’s Connect</span></button>
+            </span>
+
             </Navbar.Collapse>
         </Container>
         </Navbar>
